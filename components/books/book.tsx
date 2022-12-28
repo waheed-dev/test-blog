5e0510e99f37edcc693dbdf3ffb499d2619e837b
abdul waheed <a.waheed3738@gmail.com>
2022-12-28 22:57:58 +0500

    added books section

--- components/books/book.tsx
+++ components/books/book.tsx
@@ -1,34 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
-import {portfolios} from "./portfolioData";
 import {FunctionComponent} from "react";
 import {Boook} from "../../interfaces/book";
 
     type Props = {
-        book : Boook
+        book : Boook[]
     }
 const Book : FunctionComponent<Props> = ({book}) => {
     return (
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {portfolios.map((portfolio) => (
-                <div key={portfolio.slug} className="group relative">
+            {book.map((book) => (
+                <div key={book.slug} className="group relative">
                     <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
                         <Image
                             layout="fill"
-                            src={portfolio.coverImage}
+                            src={book.coverImage}
                             alt={""}
                             className="h-full w-full object-cover object-center"
                         />
                     </div>
                     <h3 className="mt-6 text-sm text-gray-500">
-                        <Link href={`/books/${portfolio.slug}`}>
+                        <Link href={`/books/${book.slug}`}>
                             <a>
                                 <span className="absolute inset-0" />
-                                { portfolio.title }
+                                { book.title }
                             </a>
                         </Link>
                     </h3>
-                    <p className="text-base font-semibold text-gray-900">{ portfolio.description }</p>
+                    <p className="text-base font-semibold text-gray-900">{ book.description }</p>
                 </div>
             ))}
         </div>
