26f1281c74e95821b804cfc5b8c652ae40c8f494
abdul waheed <a.waheed3738@gmail.com>
2022-12-28 23:12:03 +0500

    added books section

--- interfaces/book.ts
+++ interfaces/book.ts
@@ -1,1 +1,8 @@
-export {}
+import {Markdown} from "./markdown";
+
+export interface Boook  extends Markdown {
+    author: string;
+    coverImage : string;
+    category : string
+    downloadLink : string
+}
